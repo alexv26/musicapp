b34d9958e069a1f1aa7b92e61e2f9d149c338adf
alexv26 <velsmida@bc.edu>
2025-07-24 12:56:38 -0400

    Added colors to index.css :root

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
 
   return (
     <>
-      <div className="min-h-screen bg-[#002137] flex items-center justify-center">
-        <div className="bg-[#00263f] text-[#e0e1dd] p-8 rounded-2xl shadow-lg max-w-md w-full">
+      <div className="min-h-screen bg-[var(--color-background)] flex items-center justify-center">
+        <div className="bg-[var(--color-box)] text-[var(--color-text)] p-8 rounded-2xl shadow-lg max-w-md w-full">
           <h1 className="text-2xl font-bold mb-4">Header</h1>
           <p className="text-base">Body text</p>
         </div>
